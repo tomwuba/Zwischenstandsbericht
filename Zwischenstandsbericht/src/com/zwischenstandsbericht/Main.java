9ad2bd63bc1b9107bc373246adc865b7fe844aa9
tom_wuba <tomminecraftplay@gmail.com>
2023-04-26 18:59:36 +0200

    Edited Main

--- Zwischenstandsbericht/src/com/zwischenstandsbericht/Main.java
+++ Zwischenstandsbericht/src/com/zwischenstandsbericht/Main.java
@@ -1,16 +1,27 @@
 package com.zwischenstandsbericht;
 
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import javax.swing.JFrame;
 
 public class Main extends Thread{
 	
 	JFrame jframe;
 	
+	String pathToData = "";
+	
 	
 	
 	
 	
 	public Main() {
+		try {
+			pathToData = new File(URLDecoder.decode(Main.class.getProtectionDomain().getCodeSource().getLocation().getPath() + "/..", "UTF-8")).getAbsolutePath();
+		} catch (UnsupportedEncodingException e2) {
+			e2.printStackTrace();
+		}
 		JFrame jframe = new JFrame("Zwischenstandsbericht");
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setVisible(true);
@@ -31,6 +42,11 @@ public class Main extends Thread{
 	
 	public static void main(String[] args) {
 		new Main();
+	}
+	
+	
+	public String getPathToData() {
+		return pathToData;
 	}
 
 }
