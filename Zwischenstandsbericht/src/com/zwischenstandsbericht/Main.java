0cf2b183a264ad19bef6c66865ff2226356230df
tom_wuba <tomminecraftplay@gmail.com>
2023-04-26 18:57:20 +0200

    Edited Main

--- Zwischenstandsbericht/src/com/zwischenstandsbericht/Main.java
+++ Zwischenstandsbericht/src/com/zwischenstandsbericht/Main.java
@@ -1,5 +1,36 @@
 package com.zwischenstandsbericht;
 
-public class Main {
+import javax.swing.JFrame;
+
+public class Main extends Thread{
+	
+	JFrame jframe;
+	
+	
+	
+	
+	
+	public Main() {
+		JFrame jframe = new JFrame("Zwischenstandsbericht");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setVisible(true);
+		this.start();
+	}
+	
+	
+	@Override
+	public void run() {
+		while(true) {
+			jframe.getContentPane().repaint();
+		}
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		new Main();
+	}
 
 }
